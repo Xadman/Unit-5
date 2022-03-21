1319f2333c0bada51195308778b9496c92141c58
Xadman <90847817+Xadman@users.noreply.github.com>
2022-03-21 10:18:37 -0500

    added scores to objects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,14 +10,18 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     private int score;
     private float spawnRate = 1.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: "+ score;
-    }
+        UpdateScore(0);
+        
+        }
+
+
 
     // Update is called once per frame
     void Update()
@@ -33,6 +37,12 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            
         }
+    }
+     public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -7,17 +7,21 @@ public class Target : MonoBehaviour
 /// all the variables with the set perameters
 /// </summary>
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 14;
     private float maxTorque = 10;
     private float xRange = 6;
     private float ySpawnPos = -4;
 
+    public int pointValue;
+
     // Start is called before the first frame update
     void Start()
-    {
-        targetRb = GetComponent<Rigidbody>(); // accessing the GameObjects Rigidbody
-
+    { 
+        
+        targetRb = GetComponent<Rigidbody>();// accessing the GameObjects Rigidbody
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse); // applying Force randomly to the object
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse); // applying that force on all three axis
 
@@ -36,10 +40,12 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        
     }
 
    
