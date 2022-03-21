02ad2a2e0a054884c205209a2884a0a2c71bbbe6
Xadman <90847817+Xadman@users.noreply.github.com>
2022-03-21 10:48:11 -0500

    added particles to objects

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -15,6 +15,11 @@ public class Target : MonoBehaviour
     private float ySpawnPos = -4;
 
     public int pointValue;
+    public ParticleSystem explosionParticle;
+   
+    
+        
+    
 
     // Start is called before the first frame update
     void Start()
@@ -40,6 +45,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
